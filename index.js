9f4ad349d4018de6c504f73290164f94ab845e54
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-06-17 17:27:44 -0400

    added js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your Code Here
+
+let userName = window.prompt ('Please enter name here:')
+let userScore = 0
+let playAgain = true
+ 
+function playGame () {
+for (let i = 0; i< questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if (userAnswer === question.correctAnswer){
+ 
+        userScore = userScore + 10
+    }
+}
+ 
+window.alert('Your score is :' +  userScore)
+ 
+}
+ 
+playGame()
+ 
+while (playAgain === true) {
+   
+    let userChoice = window.prompt ('Would you like to play again? Answer yes or no')
+ 
+    if (userChoice === 'yes') {
+        playGame ()
+    }
+    else {
+       window.alert ('Game has ended')
+    }
+}
+
